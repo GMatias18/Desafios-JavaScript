8ac412849ea2d6e50f1184d33fc6831a772a7d15
GMatias18 <94495441+GMatias18@users.noreply.github.com>
2022-03-09 00:42:10 -0300

    Create Desafio 01.js

---
+++ Desafio 01/Desafio 01.js
@@ -0,0 +1,14 @@
+function Botão1(){
+    
+    window.alert("Você clicou no botão 1")
+}
+function Botão2(){
+    
+    window.alert("Você clicou no botão 2")
+}
+function Botão3(){
+    
+    window.alert("Você clicou no botão 3")
+}
+© 2022 GitHub, Inc.
+Terms
